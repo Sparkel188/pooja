7b1e9d503f1fe7c980d9160a420cb006f4e461bc
Sparkel188 <141134955+Sparkel188@users.noreply.github.com>
2023-08-19 21:23:15 +0530

    Added ArrayPuzzle

--- Assignment_3/ArrayPuzzle.js
+++ Assignment_3/ArrayPuzzle.js
@@ -1,1 +1,60 @@
+/* 
+Q4. Product array puzzle
 
+Given an array of integers A, find and return the product array of the same size where the ith element of the product array will be equal to the product of all the elements divided by the ith element of the array.
+
+Note: It is always possible to form the product array with integer (32 bit) values. Solve it without using the division operator.
+
+
+Input Format
+
+The only argument given is the integer array A.
+Output Format
+
+Return the product array.
+Constraints
+
+2 <= length of the array <= 1000
+1 <= A[i] <= 10
+For Example
+
+Input 1:
+    A = [1, 2, 3, 4, 5]
+Output 1:
+    [120, 60, 40, 30, 24]
+
+Input 2:
+    A = [5, 1, 10, 1]
+Output 2:
+    [10, 50, 5, 50]
+*/
+
+function productArray(A) {
+    const n = A.length;
+    const product = Array(n).fill(1); 
+    let leftProduct = 1;
+    let rightProduct = 1;
+  
+    
+    for (let i = 0; i < n; i++) {
+      product[i] *= leftProduct;
+      leftProduct *= A[i];
+    }
+  
+    
+    for (let i = n - 1; i >= 0; i--) {
+      product[i] *= rightProduct;
+      rightProduct *= A[i];
+    }
+  
+    return product;
+  }
+  
+  // Test case 1
+  const A1 = [1, 2, 3, 4, 5];
+  console.log(productArray(A1)); 
+  
+  // Test case 2
+  const A2 = [5, 1, 10, 1];
+  console.log(productArray(A2)); 
+  
