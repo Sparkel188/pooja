66f9c8bec23421439dd4944d8f4aba7ffdf960b2
Sparkel188 <141134955+Sparkel188@users.noreply.github.com>
2023-08-19 21:28:19 +0530

    Update Array.js

--- Assignment_2/Array.js
+++ Assignment_2/Array.js
@@ -1,1 +1,16 @@
+// Declare an array and print all values in an array using loops - for, while — Use functions
+// using for loop
+let Array = [1, 2, 3, 4, 5, 6];
+for (let i = 0; i < Array.length; i++)
+{
+    console.log(Array[i]);
+}
 
+// using while loop
+let Array_1 = [1, 2, 3, 4, 5, 6];
+let i=0;
+while(i < Array.length)
+{
+    console.log(Array[i]);
+    i++;
+}
